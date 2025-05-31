31782f416daac5748e2d3ea83582ce4c0e43e2d6
Eugene Afriyie <groupeight00@gmail.com>
2025-05-31 20:15:10 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="flexjustify-center items-center h-screen bg-blue-500">
+      <h1 className="text-xl text-white font-bold">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+export default App;

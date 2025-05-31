0e2160444435485826f292a187b653b0651c6db9
Eugene Afriyie <groupeight00@gmail.com>
2025-05-31 20:38:29 +0000

    want to deploy online

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="flexjustify-center items-center h-screen bg-blue-500">
+    <div className="flex justify-center items-center h-screen bg-blue-500">
       <h1 className="text-xl text-white font-bold">Hello, Tailwind CSS!</h1>
     </div>
   );

09b5f7ccf58641458e4849b329ca078405e9fb02
Eugene Afriyie <groupeight00@gmail.com>
2025-06-04 18:03:32 +0000

    insert the theplacehilder with the exact details

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,14 +9,14 @@ function About() {
     "We believe that small acts of charity can create a ripple effect of positive change. Whether it’s providing iftar meals during Ramadan or empowering women with vocational skills, our mission is to serve humanity and reflect the values of unity and care. Join us on this journey to make a difference and support the path to Jannah for all."
   ];
 
-  const previewText = fullText[0].slice(0, 150) + "...";
+  const previewText = fullText[0].slice(0, 950) + "...";
 
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
   };
 
   return (
-    <article id="about" className="container mx-auto py-8 sm:py-12 px-4 max-w-5xl">
+    <article id="about" className="container mx-auto py-8 sm:py-12 px-4 ma-w-5xl">
       <header className="text-center mb-6 sm:mb-8">
         <h2 className="text-3xl sm:text-4xl font-serif font-bold text-teal-800 mb-4">About Us</h2>
         <div className="border-t-4 border-teal-800 w-20 sm:w-24 mx-auto"></div>
@@ -34,9 +34,9 @@ function About() {
           </div>
           <div className="flex items-center">
             <img
-              src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&auto=format&fit=crop&w=600&h=400"
+              src="https://scontent.facc1-1.fna.fbcdn.net/v/t39.30808-6/472206263_8807425239293997_4094478365450783416_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=2285d6&_nc_eui2=AeFgYvaMkYwsdtWa3NtNCa2UEYhCQPwVae0RiEJA_BVp7W2KnehFDQ-xDrdjeeJYwmeTeIUkk1kh6XXtX6kQj77A&_nc_ohc=YIIHG1fnkR8Q7kNvwE3kmqa&_nc_oc=Adn5505DgHbOcAaf1sX04cpTOqnTjJTdBjK6cFJKxIebQfJpdJdXJgalLUQBvnM6Pmc&_nc_zt=23&_nc_ht=scontent.facc1-1.fna&_nc_gid=UM72uAG9yYsDzDFNmH7F-w&oh=00_AfK_va7R-6J6C92vlUS80PixzwcDFVQWyxHX_E6uljpIjQ&oe=68465979"
               alt="Community Support"
-              className="w-full h-64 sm:h-80 object-cover rounded-lg shadow-md"
+              className="w-full h-40 sm:h-80 object-cover rounded-lg shadow-md"
             />
           </div>
         </div>
@@ -71,7 +71,7 @@ function About() {
               <div className="border-t-2 border-teal-800 w-20 sm:w-24 mx-auto mb-6 sm:mb-8"></div>
               <div className="mb-4 sm:mb-6">
                 <img
-                  src="https://images.unsplash.com/photo-1516321310762-4794370e6a87?ixlib=rb-4.0.3&auto=format&fit=crop&w=150&h=150"
+                  src="https://scontent.facc1-1.fna.fbcdn.net/v/t39.30808-6/472206263_8807425239293997_4094478365450783416_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=2285d6&_nc_eui2=AeFgYvaMkYwsdtWa3NtNCa2UEYhCQPwVae0RiEJA_BVp7W2KnehFDQ-xDrdjeeJYwmeTeIUkk1kh6XXtX6kQj77A&_nc_ohc=YIIHG1fnkR8Q7kNvwE3kmqa&_nc_oc=Adn5505DgHbOcAaf1sX04cpTOqnTjJTdBjK6cFJKxIebQfJpdJdXJgalLUQBvnM6Pmc&_nc_zt=23&_nc_ht=scontent.facc1-1.fna&_nc_gid=UM72uAG9yYsDzDFNmH7F-w&oh=00_AfK_va7R-6J6C92vlUS80PixzwcDFVQWyxHX_E6uljpIjQ&oe=68465979"
                   alt="Community Hope"
                   className="w-20 h-20 sm:w-32 sm:h-32 object-cover rounded-md shadow-md float-right ml-3 sm:ml-4 mt-1"
                 />
@@ -82,7 +82,7 @@ function About() {
               </div>
               <div className="my-6 sm:my-8">
                 <img
-                  src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&auto=format&fit=crop&w=600&h=300"
+                  src="https://external.facc6-1.fna.fbcdn.net/emg1/v/t13/7758932051959835263?url=https%3A%2F%2Fcitinewsroom.com%2Fwp-content%2Fuploads%2F2024%2F06%2FWhatsApp-Image-2024-06-09-at-9.52.33-PM.jpeg&fb_obo=1&utld=citinewsroom.com&stp=c0.5000x0.5000f_dst-jpg_flffffff_p500x261_q75_tt6&_nc_gid=dowWGy-xWCg29b1Mdn8y-g&_nc_eui2=AeHbVpz2BCZEtxDgSxiylu0qGxky6_77L2kbGTLr_vsvaX6asx_L0npTnX3BSwJDrXjWrFqA068tQWaog3RXpxr1&_nc_oc=AdmF4omSZ4skxKm77y90tdwBBpnzvgZaZ6j6x5IynVfLkTW5ILZ2_g5FMRKVScwIZOo&ccb=13-1&oh=06_Q3-zAfq5T4UVXEVRPTtFGmj8Dcw8Y3oFuwUev4QHE3y2QphV&oe=68424A7B&_nc_sid=085657"
                   alt="Community Outreach"
                   className="w-full h-40 sm:h-48 object-cover rounded-lg shadow-md mx-auto"
                 />
@@ -93,11 +93,11 @@ function About() {
               <div className="mb-4 sm:mb-6">
                 <p className="leading-relaxed text-justify mb-4 sm:mb-0">{fullText[1]}</p>
                 <img
-                  src="https://images.unsplash.com/photo-1521791136064-7986c2920216?ixlib=rb-4.0.3&auto=format&fit=crop&w=150&h=150"
+                  src="https://scontent.facc6-1.fna.fbcdn.net/v/t39.30808-6/494218817_9489761007727080_5164433794427945810_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=102&ccb=1-7&_nc_sid=536f4a&_nc_eui2=AeHWgBj_ScLPZtAkKgXgS2I3u1t0bWHnKMS7W3RtYecoxKhy8B7NdHswEjMMyNSnW3QVU9PavISHGDd9rMWnza4B&_nc_ohc=PTgVcLdEKssQ7kNvwGqfwf2&_nc_oc=AdlDp9Bt_R3sUI8fdq88AlHH6vWuuEVe8Tvf_uI0-czlRnE2uZDtx8Z3S3x42m61hLg&_nc_zt=23&_nc_ht=scontent.facc6-1.fna&_nc_gid=t5-iizum22jx2vJcDWN_lA&oh=00_AfK59DMvisB6cS5mc2X7As_fXGDwxaa0yQQlznm-tU_AMQ&oe=684656BC"
                   alt="Community Unity"
                   className="w-20 h-20 sm:w-32 sm:h-32 object-cover rounded-md shadow-md float-left mr-3 sm:mr-4 mt-1"
                 />
-                <p className="leading-relaxed text-justify">
+                <p className="leading-relaxed text-justify first-letter:text-3xl sm:first-letter:text-4xl first-letter:font-bold first-letter:text-teal-800 first-letter:float-left first-letter:mr-2 sm:first-letter:mr-3">
                   {fullText[2]}
                 </p>
                 <div className="clear-both sm:clear-none"></div>

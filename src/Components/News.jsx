8265b5faf0a3dbebcf969ef39e6b9ce13179c2b2
Eugene Afriyie <groupeight00@gmail.com>
2025-06-04 09:26:49 +0000

    the lg text absolute on the new sec

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -3,9 +3,9 @@ function News({ featuredNews, news, openModal, updateFeaturedNews }) {
     <div id="news" className="container mx-auto py-8 px-4">
       <h2 className="text-3xl font-bold text-center text-teal-800 mb-6">Latest News</h2>
       <p className="text-center text-gray-700 mb-6">Stay updated with our recent activities and impact.</p>
-      <div className="grid md:grid-cols-3 gap-6">
+      <div className="grid md:grid-cols-3 gap-6 h-[500px]">
         {featuredNews && (
-          <div className="md:col-span-2 relative bg-gray-100 rounded-lg overflow-hidden shadow-md group">
+          <div className="md:col-span-2 relative bg-gray-100 rounded-lg overflow-hidden shadow-md group ">
             <img src={featuredNews.image} alt={featuredNews.alt} className="w-full h-[500px] object-cover transition duration-300 group-hover:scale-105 group-hover:brightness-75" />
             <div className="absolute bottom-0 bg-black bg-opacity-50 text-white p-6 w-full transition duration-300 group-hover:-translate-y-2">
               <p className="text-sm bg-teal-200 inline-block px-2 py-1 text-teal-800 font-bold mb-2">{featuredNews.category}</p>
@@ -17,7 +17,7 @@ function News({ featuredNews, news, openModal, updateFeaturedNews }) {
           </div>
         )}
         {news.length > 1 && (
-          <div className="space-y-4 max-h-[100%] overflow-y-hidden pr-2">
+          <div className="space-y-4 max-h-[100%] overflow-y-auto pr-2">
             {news.map((item, index) => item !== featuredNews && (
               <div key={index} className="bg-white p-4 rounded-lg shadow-md hover:bg-teal-50 transition animate-fadeInUp" style={{ animationDelay: `${index * 0.2}s` }}>
                 <p className="text-xs bg-teal-200 inline-block px-2 py-1 text-teal-800 font-bold mb-1">{item.category}</p>

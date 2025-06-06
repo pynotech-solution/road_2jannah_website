d225aba2a6eb1d07c51f34bc41d9bfb623ce9893
Eugene Afriyie <groupeight00@gmail.com>
2025-06-06 17:59:54 +0000

    the gallery padding

--- src/Components/Gallery_Con.jsx
+++ src/Components/Gallery_Con.jsx
@@ -2,7 +2,7 @@ import GalleryItem_Con from './GalleryItem_Con.jsx';
 
 function Gallery_Con({ gallery, openGalleryModal }) {
   return (
-    <div id="gallery" className="container mx-auto py-8">
+    <div id="gallery" className="container mx-auto py-8 px-2">
       <h2 className="text-3xl font-bold text-center text-teal-800 mb-6">Our Impact in Photos</h2>
       <p className="text-center text-gray-700 mb-6">See the difference your donations make in the lives of those we serve.</p>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6">

964dd4ad671495694a332095bf5e34ee4a95775e
Eugene Afriyie <groupeight00@gmail.com>
2025-06-12 15:59:15 +0000

    donate img

--- src/Components/Donate_Con.jsx
+++ src/Components/Donate_Con.jsx
@@ -13,7 +13,7 @@ function Donate_Con({ openDonationModal }) {
           </div>
         </div>
         <div>
-          <img src="https://scontent.facc6-1.fna.fbcdn.net/v/t39.30808-6/496940432_9621165381253308_5638857211374510766_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=103&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeHoD6FckslT8lnzbi9AlpdBjpzEvfjxzXiOnMS9-PHNeMh4K_3HXwUiu7SNAf2XaDR_-ElfTLd6ffp_xc6OGC-u&_nc_ohc=v6mAellPu4oQ7kNvwGECHJm&_nc_oc=AdmYrrfDSg88vKwtaP7Nj0Yb2Ej3vKQc_7wU-nb1BjdiCCcHUGSS-h9roBh8Ks--QbA&_nc_zt=23&_nc_ht=scontent.facc6-1.fna&_nc_gid=ox7ZhqW_IttcmguMQDLHiA&oh=00_AfPUjuOfwBn_75z9uZt6r7mi06jYwyhXNYkgrNwEsel7Mw&oe=6847F384" alt="Donate Now Community Support" className="w-full h-96 object-cover rounded-lg" />
+          <img src="https://i.ibb.co/gLjwKGgY/image.png" alt="Donate Now Community Support" className="w-full h-96 object-cover rounded-lg" />
         </div>
       </div>
     </div>

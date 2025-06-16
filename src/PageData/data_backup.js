c47e43b9b51b3d13d885f8d8fa23876e48dc7e53
Eugene Afriyie <groupeight00@gmail.com>
2025-06-16 03:45:18 +0000

    FILING THE DATA

--- src/PageData/data_backup.js
+++ src/PageData/data_backup.js
@@ -65,6 +65,133 @@
            
                 ]
                },
+    {type: "album",
+        description:'',
+        title: "R2J donate to Mampong School of blind",
+        date: "27/06/2024",
+          media:[
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750036912/MAMPONG_SCHOLL_tanino.jpg",
+            },
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750036912/7758932051959835263_kbahzt.jpg",
+            },
+          ]
+        
+        
+        },
+    {type: "album",
+        description:"R2J Pays Courtesy Call To RCO'24 Stakeholders The courtesy call which saw Road 2 Jannah Foundation led by it's Executive Director/Founder,  presented Citation to stakeholders of the just ended Ramadan Community Outreach Phase 2 Project.This gesture was to appreciate their support and be enlightened more in the humanitarian sector.The foundation first visited Dawadawa Jollof Delight @Adenta where conversations about empowerment took the center stage.The CEO of Dawadawa Jollof Delight, Rabiatu Atule Akamim observed from her findings on gender equality in the zongo communities that Representation of Muslim women in decision making is almost nonexistent and some Muslim women are very timid at decision making table and has therefore charged the group to facilitate a sensitization program that will see to empower the Moslem Women to be vocal at decision making table.The group then moved to cantonments where they joined a moslem congregation, a prayer led by Imam of National Police Mosque Chief Supt. of Police Imam Hussein Abdul Rahim Hussein in Jumah prayers.Imam Hussein Abdul Rahim Hussein and his deputy Imam Abass Abdul-Karim then engaged the foundation in a close door meeting after prayers where he invoke the blessings of Almighty Allah and spirit of unity in the group.Imam Hussein Abdul Rahim Hussein asked the foundation to take a critical look in and around the communities their membership is based and make an impact of changing lives.On his part, Imam Abass Abdul-Karim mentioned about sustainability of NGO's due to lack of structural absence in their modules of operandi and advice the group to employ the act of sustainability and keep up the good work.A Citation was then presented to Imam Hussein Abdul Rahim Hussein to honour his role in positive change in soceity through his impactful sermons.Baina Tv, the official media partner for the foundation in it's operations also received a citation for supporting the course with it's platforms to reach higher heights.The last to be awarded citation was the proprietor of Rahma Orphanage Home Sheikh Yusif Musah for dedicating his life for the vulnerable kids in society.",
+        title: "R2J Pays Courtesy Call To RCO'24 Stakeholders",
+        date: "19/06/2024",
+          media:[
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750036913/1_ggg1ea.jpg",
+            },
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750036913/3_se7ga6.jpg",
+            },
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750036914/2_hlffgx.jpg",
+            },
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750036918/4_ihg5z7.jpg",
+            },
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750036943/495516026_9593036537399526_2092711318723451072_n_vrrtww.jpg",
+            },
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750036969/495654999_9593036540732859_2176995531783368908_n_o7757m.jpg",
+            },
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750037046/495804950_9593036547399525_353190617025214346_n_kxatgq.jpg",
+            },
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750037046/495805064_9593036577399522_5771634513842279882_n_jzfwvp.jpg",
+            },
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750037046/496006223_9593036527399527_3719181368562995419_n_yyffzi.jpg",
+            },
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750037046/496254434_9593036554066191_1275625130246270772_n_olxsjb.jpg",
+            },
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750037047/496608118_9593036544066192_7072624773951178764_n_n7eaja.jpg",
+            },
+          ]
+        
+        
+        },
+    {type: "album",
+        description:"R2J, KGL Foundation Donates To Akropong School For The Blind And Chief Imam* The three-day outreach dubbed the *Ramadan Community Outreach* by *Road 2 Jannah and KGL  Foundation* commenced on Monday 17th April, 2023.The group made it first donation to the *Akropong School For The Blind.* Items donated included bags of rice, cartons of milk, packs of brown sugar, packs of bottled water and carbonated drinks, cooking oil, boxes of Lipton, clothes, shoes and sandals.In a speech delivered on behalf of the two foundations, the CEO of KGL Foundation Mr Elliot Dadey, made it clear to the students and management that his outfit and *Road 2 Jannah Foundation* will continue to support the school even beyond the Ramadan period. Receiving the donation on behalf of management and students of the school, Mr Sowuh said, they are grateful for their inclusion in this year's outreach and has called on other benovolent organizations and individuals to support this great initiative.The team then moved to the Chief Imam's Residence in Accra to donate similar items. This was in honour of a promise made to the Chief Imam when he was presented with a citation last week.On its part, *Road 2 Jannah Foundation* expressed appreciation to all donors and called for more support.The outreach takes another turn to the Weija Leprosarium and Abossey Okai Zongo Tuesday 18th April, 2023 on Day 2 and Wednesday 19th April, 2023 at Rahma Orphanage Home Alajo to climax the outreach on day 3.",
+        title: "R2J, KGL Foundation Donates To Akropong School For The Blind And Chief Imam",
+        date: "18/04/2023",
+          media:[
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750037048/1_zd6og9.jpg",
+            },
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750037048/10_mlobea.jpg",
+            },
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750037049/3_dlwvub.jpg",
+            },
+          
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750037049/4_pj7vdz.jpg",
+            },
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750037049/2_xdx3cq.jpg",
+            },
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750037049/2_xdx3cq.jpg",
+            },
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750037052/11_lelmj1.jpg",
+            },
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750037071/5_bfuxqk.jpg",
+            },
+          
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750037099/6_esbhwb.jpg",
+            },
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750037397/8_wckegn.jpg",
+            },
+            {
+                type: "image",
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750037397/9_yz0ima.jpg",
+            },
+          
+        
+          ]
+        
+        
+        }
 
 
 

3925a890ea56621c8b7985dd671a558d1fe88582
Eugene Afriyie <groupeight00@gmail.com>
2025-06-16 03:22:15 +0000

    first data

--- src/PageData/data_backup.js
+++ src/PageData/data_backup.js
@@ -2,42 +2,45 @@
    export const gallery = [
 
     { type: "album",
-       src: "https://scontent.facc6-1.fna.fbcdn.net/v/t39.30808-6/494135333_9503102626392918_2278131271513998804_n.jpg?stp=dst-jpg_s600x600_tt6&_nc_cat=103&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeFV31yFYOSEr4uDC_Nptrq6zSBa5RQEvS3NIFrlFAS9LSHtonfRoxCXQoCKiMxfIF4-kqwlYjp6u792dXl2RbdM&_nc_ohc=NlBf2EUE480Q7kNvwHm5sjI&_nc_oc=Adm32OMzOCSvgTqBKMPzibLKgzGANMkkMShodCIUXc20SWxEynxE12jR9QJYo6QIbv4&_nc_zt=23&_nc_ht=scontent.facc6-1.fna&_nc_gid=tQ1gRnfb5KdX9Bn7oumyOQ&oh=00_AfNxMzHyz-Ei1gsUQDqI9ZmUlM3KREr4YkD-I9P5gLPcOA&oe=6847E0F1",
-        alt: "Medwuma Pa Album",
-         caption: "The Road 2 Jannah Foundation from New Bakpa made a stop at *Anyamam* in the Ada West District to donate to the affected tidal waves victims rendered homeless seeking shelter in churches and schools as part of the communities to reach out to.",
-          items:
+            title: "Road 2 Jannah Foundation's RCO'24 in Anyamam",
+        description:"Road 2 Jannah Foundation's RCO'24 in Anyamam in the Ada West District of Greater Accra Region The Road 2 Jannah Foundation from New Bakpa made a stop at *Anyamam* in the Ada West District to donate to the affected tidal waves victims rendered homeless seeking shelter in churches and schools as part of the communities to reach out to.The month of Ramadan is a month to show compassion and love to the needy and vulnerable.  The foundation donated items provided by cooperate organizations and benevolent donors like Rice, Oil, Maize, Clothes and Exercise Books along with a number of other items to the Community.Hon. Philip Sai, the Assembly member of Anyamam welcome the gesture, lauded  R2J foundation on behalf of victims of the tidal waves for it's commitment in the quest to support vulnerable communities and urge co-operate organisations to continue supporting Road 2 Jannah Foundation do more.In his address, Hon. Philip Sai called on government to speed up on constructing the sea defence in the area to avoid further destruction of properties and possible lost of lives.Let's make a Change together by donating any amount to support RCO'24 Phase Two Project The outreach was supported by Wilmar Gh, NLA Good Causes Foundation, Dawadawa Jollof Delight and Kingdom of Saudi Arabia " ,
+          media:
            [
             
             { type: "image", 
-                src: "https://scontent.facc6-1.fna.fbcdn.net/v/t39.30808-6/494135333_9503102626392918_2278131271513998804_n.jpg?stp=dst-jpg_s600x600_tt6&_nc_cat=103&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeFV31yFYOSEr4uDC_Nptrq6zSBa5RQEvS3NIFrlFAS9LSHtonfRoxCXQoCKiMxfIF4-kqwlYjp6u792dXl2RbdM&_nc_ohc=NlBf2EUE480Q7kNvwHm5sjI&_nc_oc=Adm32OMzOCSvgTqBKMPzibLKgzGANMkkMShodCIUXc20SWxEynxE12jR9QJYo6QIbv4&_nc_zt=23&_nc_ht=scontent.facc6-1.fna&_nc_gid=tQ1gRnfb5KdX9Bn7oumyOQ&oh=00_AfNxMzHyz-Ei1gsUQDqI9ZmUlM3KREr4YkD-I9P5gLPcOA&oe=6847E0F1",
-                 alt: ""
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750036908/1_hvutom.jpg",
               }, 
             { type: "image", 
-                src: "https://scontent.facc1-1.fna.fbcdn.net/v/t39.30808-6/494753965_9503102479726266_7784063683569608253_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeHvq9MS_um_5hePj7FocWfXXv9gDSEDNDpe_2ANIQM0Og5Ilc0XWIk1GYg0t-PP6axUM8UL_fyFczetjnecJNY8&_nc_ohc=qNh6YWfZyj4Q7kNvwF7lDny&_nc_oc=Adl710XNV-wBWmdEdgCRTjYctRdtoXJKzViL2u6TfAh5QBZnZYlm_ImvHm--XpdilTY&_nc_zt=23&_nc_ht=scontent.facc1-1.fna&_nc_gid=V7zisK85aubh6TqmbCr8MA&oh=00_AfPFjiZCvhraOt6q2ZGheYdm3DqAOz4e9J2w8Nbh2CbUFg&oe=6847F47E",
-                 alt: ""
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750036908/4_jy3qjv.jpg",
               }, 
             { type: "image", 
-                src: "https://scontent.facc1-1.fna.fbcdn.net/v/t39.30808-6/495016068_9503102423059605_6915978106904892275_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeFGVgwMP_Dl1n0HBcK7bftZTCSDmstyEsZMJIOay3ISxjDcac2WXRAZsSugpqVeCvw0xgNYHtujn1uKQ4PdlCi_&_nc_ohc=MkknYNEqg7MQ7kNvwGitWuJ&_nc_oc=AdkNcLTk4wMkO9SV_w8RefyXajOztPD7lY9kdEk0CCpT2oZZnShi05LWCb3eqc_LPJM&_nc_zt=23&_nc_ht=scontent.facc1-1.fna&_nc_gid=zM1dQt3-eyRdjybAPTz4Kg&oh=00_AfM4SgRXYC5Kx5XW3tuZ0rEIoHhqFfYYY8q0CK0noP6wOQ&oe=6847F017",
-                 alt: ""
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750036908/3_h6b4mt.jpg",
               }, 
-
+       
             { type: "image", 
-                src: "https://scontent.facc6-1.fna.fbcdn.net/v/t39.30808-6/494517444_9503102629726251_5712526822170111483_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeHLsxnDmqvXHi6Z73xKaQ4qXLFSKJFwVXJcsVIokXBVcn6EJPBObWzEBAIW1ysHJ_2hgJpceD1j1VJ37gt_s0q0&_nc_ohc=OgcKpFoqYHQQ7kNvwGyOVLV&_nc_oc=AdkZEvHMgIUAaqqLAnd77IZxUHWP9SX9Jwo6tCS8R-4WWKf_-58hoyFFIetphjJxPbc&_nc_zt=23&_nc_ht=scontent.facc6-1.fna&_nc_gid=9hxJu52d5IEZy-93Nbv3lw&oh=00_AfNlX5J5QqVkKUzKKYpxG28VooIXEC8SVHqh_2g13lOmtw&oe=6847E056",
-                 alt: ""
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750036908/2_h8tymp.jpg",
               }, 
+       
             { type: "image", 
-                src: "https://scontent.facc6-1.fna.fbcdn.net/v/t39.30808-6/495009617_9503102596392921_8259024072094532516_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeEkemfI62AKTtNHPevqvhDQes5uwdCRWGp6zm7B0JFYaqttlm3KI0ZTvnyKUhlkrR47N5kKcgOFqqY143Wt8CBY&_nc_ohc=ONkTmH0os_0Q7kNvwEzKK7j&_nc_oc=AdlVBpy1R6rLH3wXWSpNr_vrtH468GDyfCD_28WR9FK82Xews-Vq6oIzlSATN6S-8PU&_nc_zt=23&_nc_ht=scontent.facc6-1.fna&_nc_gid=ocXkqWClJtEtNoV7X0YqtQ&oh=00_AfPaUy3bebeXva5hxT56GlfEKLH5xnpFRI_rjBs1LscXIA&oe=68480442",
-                 alt: ""
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750036909/7_mxk0qr.jpg",
               }, 
             { type: "image", 
-                src: "https://scontent.facc6-1.fna.fbcdn.net/v/t39.30808-6/494283129_9503102446392936_8118175293162152504_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeGFKmbqf9jifL6iYReBYt0zSCDOYFWl6l9IIM5gVaXqX8mW7Vnl-Yq1t1CBrBfW-oIg2e4ug42N8t1i5p6VV1jr&_nc_ohc=lsL78b1zbw4Q7kNvwG31HA1&_nc_oc=AdmW7ndsZSqpsgEZy0KNum6ff7-6i-Xk9GMIa7A_mFUI7Gm_Yz_iEdsdWPaQuE5b4f8&_nc_zt=23&_nc_ht=scontent.facc6-1.fna&_nc_gid=mIw4ZXitKaJNthnAGETp7w&oh=00_AfMVQ801mYYtv01spknJggXUZWtwJkYpvlG2tDw7YhBOVw&oe=68480505",
-                 alt: ""
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750036909/6_zojzcf.jpg",
               }, 
             { type: "image", 
-                src: "https://scontent.facc6-1.fna.fbcdn.net/v/t39.30808-6/494169659_9503102486392932_2462335850931146697_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeFwcReRJ0hvlpFuuxH60-TaY5yX2fabPUdjnJfZ9ps9R0kArxdvXV9zfF3aKpKtM2uE4nilCrWekfFILdNZTz6F&_nc_ohc=kF1xrquhjn0Q7kNvwHbiGbg&_nc_oc=AdkA1DdPj2mN-4CdAZ1BMBdsPzj0qAVd9Pzy6FVnZXwkClcjw4LNmS0nQzGYE3TKTlo&_nc_zt=23&_nc_ht=scontent.facc6-1.fna&_nc_gid=BBsGW97DtW-INPreSZ5TnQ&oh=00_AfNQpxi4mP_l12p-YKQUEJlY4pkoo7lF0uesxxRXB0keLg&oe=684803D3",
-                 alt: ""
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750036909/5_feotv4.jpg",
               }, 
-
+            { type: "image", 
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750036910/9_ctjunk.jpg",
+              }, 
+            { type: "image", 
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750036909/8_hfgkdd.jpg",
+              }, 
+       
+            { type: "image", 
+                src: "https://res.cloudinary.com/dzqdfaghg/image/upload/v1750036908/10_qzuxto.jpg",
+              }, 
+       
             
            
                 ]
